e55f07c53d3dba4ea8b3b2f60306fe13ea2b3824
nitheeshmavila <mail2nitheeshmavila@gmail.com>
2016-11-19 19:46:09 +0530

    added brush

--- funpaint.js
+++ funpaint.js
@@ -1,30 +1,68 @@
+var down = false;
+var x_pos;
+var y_pos;
+var canvas = document.getElementById("canvas1");
+var ctx = canvas.getContext("2d");
 
-var draw_line = function()
-{	
-	ctx.beginPath();
-	ctx.moveTo(0, 0);
-	ctx.lineTo(138, 250);
-	ctx.stroke();
+
+var get_coordinates = function()
+{
+	var x_pos = e.clientX - canvas.offsetLeft;
+        var y_pos = e.clientY - canvas.offsetTop;
+	return {x: x_pos, y: y_pos};
 }
 
-var clear_canvas = function()
+var brush_draw = function()
+{
+	canvas.addEventListener('mousedown', function()
+	{
+		down = true;
+		ctx.beginPath();
+		ctx.moveTo(x_pos,y_pos);
+		canvas.addEventListener('mousemove', draw);
+	});
+
+	canvas.addEventListener('mouseup', function()
+	{
+		down = false;
+	});
+	var draw = function()
+	{
+		var brush_size = document.getElementById("brushsize").value;
+		var brush_color  = document.getElementById("brushcolor").value;
+        	ctx.fillStyle = brush_color;
+        	ctx.strokeStyle = brush_color;
+		ctx.lineWidth = brush_size;
+		var x_pos = e.clientX - canvas.offsetLeft;
+        	var y_pos = e.clientY - canvas.offsetTop;
+		if(down == true){
+			ctx.lineTo(x_pos, y_pos);
+			ctx.stroke();
+		}
+	}
+
+
+}
+
+var clear_canvas = function(e)
 {
         ctx.clearRect(0, 0, canvas.width, canvas.height);
 }
 
-
-var get_coorrdinate = function(event)
+var fill_canvas = function(e)
 {
-	var x = event.clientX - canvas.getBoundingClientRect().left;
-        var y = event.clientY - canvas.getBoundingClientRect().top;
-	return {x: x, y: y};
+	var fill_color  = document.getElementById("fillcolor").value;
+	ctx.fillStyle = fill_color;
+	ctx.strokeStyle = fill_color;
+	ctx.fillRect(0, 0, canvas.width, canvas.height);
+
 }
 
 
-var canvas = document.getElementById("canvas1");
-var ctx = canvas.getContext("2d");
-document.getElementById("line").addEventListener("click", draw_line);
-document.getElementById("clear").addEventListener("click",clear_canvas);
 
+document.getElementById("fillbucket").addEventListener("click",fill_canvas);
+document.getElementById("clear").addEventListener("click",clear_canvas);
+document.getElementById("b1").addEventListener("click", brush_draw);
+document.getElementById("line").addEventListener("click", straight_line_draw);
 
 
