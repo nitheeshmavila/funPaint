e921e47fd317f35c51e05b34ac310e4e4cc4c454
nitheeshmavila <mail2nitheeshmavila@gmail.com>
2016-11-22 12:34:29 +0530

    added file open

--- funpaint.js
+++ funpaint.js
@@ -1,121 +1,107 @@
-var down = false;
-var x_pos;
-var y_pos;
+var dragging = false;
 var canvas = document.getElementById("canvas1");
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
 var ctx = canvas.getContext("2d");
+var radius = 10;
+ctx.lineWidth = radius*2;
 
-
-var get_coordinates = function()
+var get_coordinates = function(e)
 {
-	var x_pos = e.clientX - canvas.offsetLeft;
-        var y_pos = e.clientY - canvas.offsetTop;
-	return {x: x_pos, y: y_pos};
+	return{'x': e.clientX, 'y': e.clientY};
 }
 
-var get_brush_size = function()
+var get_brush_color = function()
+{
+	var color = document.getElementById('brushcolor').value;
+	return color;
+}
+
+var put_point = function(e)
 {	
-	var brush_size = document.getElementById("brushsize").value;
-	return brush_size;
+	var color = get_brush_color();
+	ctx.fillStyle = color;
+	ctx.strokeStyle = color;
+	if(dragging == true){
+		ctx.lineTo(get_coordinates(e)['x'] , get_coordinates(e)['y']);
+		ctx.stroke();
+		ctx.beginPath();
+		ctx.arc(get_coordinates(e)['x'] , get_coordinates(e)['y'],  radius, 0, Math.PI*2);
+		ctx.fill();
+		ctx.beginPath();
+		ctx.moveTo(e.clientX, e.clientY);
+	}
 }
 
-var get_color = function()
+var engage_mouse = function(e)
 {
-	var brush_color  = document.getElementById("brushcolor").value;
-	return brush_color;
+	dragging = true;
+	put_point(e);
+}
+
+var disengage_mouse = function()
+{
+	dragging = false;
+	ctx.beginPath();
 }
 
 var brush_draw = function()
 {
-	canvas.addEventListener('mousedown', function()
-	{
-		down = true;
-		ctx.beginPath();
-		ctx.moveTo(x_pos,y_pos);
-		canvas.addEventListener('mousemove', draw);
-	});
-
-	canvas.addEventListener('mouseup', function()
-	{
-		down = false;
-	});
-	var draw = function(e)
-	{
-		
-		var color = get_color();
-        	ctx.fillStyle = color;
-        	ctx.strokeStyle = color;
-		ctx.lineWidth = get_brush_size();
-		var x_pos = e.clientX - canvas.offsetLeft;
-        	var y_pos = e.clientY - canvas.offsetTop;
-		if(down == true){
-			ctx.lineTo(x_pos, y_pos);
-			ctx.stroke();
-		}
-	}
-
-
+	canvas.addEventListener('mousedown', engage_mouse);
+	canvas.addEventListener('mousemove', put_point);
+	canvas.addEventListener('mouseup', disengage_mouse);
 }
 
-var clear_canvas = function(e)
+var clear_canvas = function()
 {
         ctx.clearRect(0, 0, canvas.width, canvas.height);
 }
-
-var pencil_draw = function()
-{
-	canvas.addEventListener('mousedown', function()
-        {
-                down = true;
-                ctx.beginPath();
-                ctx.moveTo(x_pos,y_pos);
-                canvas.addEventListener('mousemove', draw);
-        });
-
-        canvas.addEventListener('mouseup', function()
-        {
-                down = false;
-        });
-	
-        var draw = function(e)
-        {
-
-                var color = get_color();
-                ctx.fillStyle = color;
-                ctx.strokeStyle = color;
-                ctx.lineWidth = 0.1;
-		ctx.lineCap = "round";
-                var x_pos = e.clientX - canvas.offsetLeft;
-                var y_pos = e.clientY - canvas.offsetTop;
-                if(down == true){
-                        ctx.lineTo(x_pos, y_pos);
-                        ctx.stroke();
-                }
-	}
-}
-          
-var fill_canvas = function(e)
+         
+var fill_canvas = function()
 {
 	var fill_color  = document.getElementById("fillcolor").value;
 	ctx.fillStyle = fill_color;
 	ctx.strokeStyle = fill_color;
 	ctx.fillRect(0, 0, canvas.width, canvas.height);
-
+	
 }
 
-document.getElementById("imagefile").addEventListener("change", function(e)
+var get_file_width = function()
 {
-	var temp = URL.createObjectURL(e.target.files[0]);
+        var file_width = document.getElementById("filewidth").value;
+        return file_width;
+}
+
+var get_file_height = function()
+{
+        var file_height = document.getElementById("fileheight").value;
+        return file_height;
+}
+
+var print_file_image = function(e)
+{	
+	clear_canvas();
+        var temp = URL.createObjectURL(e.target.files[0]);
 	var image = new Image();
 	image.src = temp;
 	image.addEventListener("load", function()
-	{
-		ctx.drawImage(image, 0, 0);
+	{	
+		var width = get_file_width();
+		var height = get_file_height();
+		ctx.drawImage(image, 0, 0, width, height);
 	});
-});
+	return;
+}
 
-document.getElementById("fillbucket").addEventListener("click",fill_canvas);
-document.getElementById("clear").addEventListener("click",clear_canvas);
-document.getElementById("b1").addEventListener("click", brush_draw);
-/*document.getElementById("line").addEventListener("click", straight_line_draw);*/
-document.getElementById("p1").addEventListener("click", pencil_draw);
 
+
+var main = function()
+{	
+	document.getElementById("b1").addEventListener("click", brush_draw);
+	document.getElementById("imagefile").addEventListener("change",print_file_image);
+	document.getElementById("fillbucket").addEventListener("click",fill_canvas);
+	document.getElementById("clear").addEventListener("click",clear_canvas);
+	
+}
+
+window.addEventListener('load', main, false);
